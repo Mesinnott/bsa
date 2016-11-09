99b98c3bf32a8f1c1caae23cb23e854cc8de9a15
Jonathan Steenblik <blikone@hotmail.com>
2016-11-09 14:05:21 -0700

    Models and Data Adapter

---
+++ server-assets/models/camp-model.js
@@ -0,0 +1,39 @@
+let dataAdapter = require('./data-adapter'),
+    uuid = dataAdapter.uuid,
+    // schemator = dataAdapter.schemator,
+    DS = dataAdapter.DS,
+    formatQuery = dataAdapter.formatQuery;
+
+
+function create(camp, cb) {
+    
+    Camp.create({ 
+        id: uuid.v4(), 
+        name: camp.name, 
+        yearId: camp.yearId,
+        location: camp.location,
+        date: camp.date,
+        weekdays: camp.weekdays,
+        campNum: camp.campNum,
+        directorId: camp.directorId,
+        startTime: camp.startTime,
+        endTime: camp.endTime,
+        scoutGroups: camp.scoutGroups
+    }).then(cb).catch(cb)
+}
+
+function getAll(query, cb) {
+    //Use the Resource Model to get all Galaxies
+    Camp.findAll({}).then(cb).catch(cb)
+}
+
+function getById(id, query, cb) {
+    // use the Resource Model to get a single star by its id
+    Camp.find(id, formatQuery(query)).then(cb).catch(cb)
+}
+
+module.exports = {
+    create,
+    getAll,
+    getById
+}
---
+++ server-assets/models/year-model.js
@@ -0,0 +1,30 @@
+let dataAdapter = require('./data-adapter'),
+    uuid = dataAdapter.uuid,
+    // schemator = dataAdapter.schemator,
+    DS = dataAdapter.DS,
+    formatQuery = dataAdapter.formatQuery;
+
+
+function create(date, cb) {
+    // Use the Resource Model to create a new planet
+    let year = { id: uuid.v4(), year: date };
+    
+    Year.create(year).then(cb).catch(cb)
+}
+
+
+function getAll(query, cb) {
+    //Use the Resource Model to get all Galaxies
+    Year.findAll({}).then(cb).catch(cb)
+}
+
+function getById(id, query, cb) {
+    // use the Resource Model to get a single star by its id
+    Year.find(id, formatQuery(query)).then(cb).catch(cb)
+}
+
+module.exports = {
+    create,
+    getAll,
+    getById
+}
