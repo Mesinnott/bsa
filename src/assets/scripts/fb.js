3598686c54670133f271effcf3163f1e95401f3e
Michael Sinnott <mesinnott@gmail.com>
2016-11-09 15:45:40 -0700

    firebase!

---
+++ src/assets/scripts/fb.js
@@ -0,0 +1,13 @@
+
+// <script src="https://www.gstatic.com/firebasejs/3.6.0/firebase.js"></script>
+// <script>
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyCDIhdp988peKG7BHnCYWQddQHg1wgnwj8",
+//     authDomain: "oreidabsa-b4054.firebaseapp.com",
+//     databaseURL: "https://oreidabsa-b4054.firebaseio.com",
+//     storageBucket: "oreidabsa-b4054.appspot.com",
+//     messagingSenderId: "699878490725"
+//   };
+//   firebase.initializeApp(config);
+// </script>
