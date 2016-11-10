492efc06a326c9c89dff4817a4a801fe447ac3d4
Michael Sinnott <mesinnott@gmail.com>
2016-11-10 16:10:50 -0700

    reservation routes

--- server-assets/routes/reservation-routes.js
+++ server-assets/routes/reservation-routes.js
@@ -1,33 +1,33 @@
 const router = require('express').Router();
-const Camp = require('../models/camp-model');
+const Reservation = require('../models/reservation-model');
 
-module.exports.mountPath = '/camps'
+module.exports.mountPath = '/reservations'
 module.exports.router = router;
 
 router.route('/:id?')
   .get(function (req, res, next) {
     if (req.params.id) {
-      Camp.getByAnyId(req.params.id, req.query.include, function (camp) {
-        if (camp.stack) { return next(camp) }
-        return res.send(camp)
+      Reservation.getByAnyId(req.params.id, req.query.include, function (reservation) {
+        if (reservation.stack) { return next(reservation) }
+        return res.send(reservation)
       })
     }
   })
   .post(function (req, res, next) {
-    Camp.create(req.body.camp, function (camp) {
-      if (camp.stack) { return next(camp) }
-      return res.send(camp)
+    Reservation.create(req.body.reservation, function (reservation) {
+      if (reservation.stack) { return next(reservation) }
+      return res.send(reservation)
     })
   })
   .put(function (req, res, next) {
-    Camp.editById(req.body.camp, function (camp) {
-      if (camp.stack) { return next(camp) }
-      return res.send(camp)
+    Reservation.editById(req.body.reservation, function (reservation) {
+      if (reservation.stack) { return next(reservation) }
+      return res.send(reservation)
     })
   })
   .delete(function (req, res, next) {
-    Camp.deleteById(req.body.camp.id, function (camp) {
-      if (camp.stack) { return next(camp) }
-      return res.send('camp' + camp.campNum + ' deleted')
+    Reservation.deleteById(req.body.reservation.id, function (reservation) {
+      if (reservation.stack) { return next(reservation) }
+      return res.send('reservation' + reservation.reservationNum + ' deleted')
     })
   })
