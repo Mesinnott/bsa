9ac3e621001b14beea2fba8512cbfc1c6f11ef1b
Tyler Gervais <cfcgervais@gmail.com>
2016-12-01 14:17:41 -0700

    tyler 21/1/06

--- src/components/campAvail-component.js
+++ src/components/campAvail-component.js
@@ -1,33 +1,37 @@
-import template from './templates/campavail.html'
-import './stylesheets/campavail.scss'
+import template from './templates/campAvail.html'
+import './stylesheets/campAvail.scss'
 const Component = 'campavail'
 // Use this as a template.
 angular.module(`app.components.${Component}`, [])
-  .service('availabilityService', function ($http) {
+  .service('avService', function ($http) {
     var av = this;
     // var url1 = 'https://bcw-getter.herokuapp.com/?url=http%3A%2F%2Fquotesondesign.com%2Fapi%2F3.0%2Fapi-3.0.json'
-    // var yearId= 'ss'
+    var yearId= '62cde5b1-e09f-41ca-b45e-ccf4c45f0df8'
     av.checkAllCamps = () => {
       $http.get('/api/camps/' + yearId)
         .then(function (res) {
           console.log(res.data)
-          av.camps = res.data
-          av.camps = av.camps.sort(function(a,b){
-            return a.date-b.date
-          })
-        })
+          return res.data
+          // av.camps = res.data
+          // av.camps = av.camps.sort(function(a,b){
+          //   return a.date-b.date
+          // })
+        }).catch(a=>console.log(a))
         return av.camps
     }
   })
-  .controller('availabilityController', function (availabilityService, $http) {
-    let $ctrl = this;
-    $ctrl.test = 'testing 123'
-    this.camps=availabilityService.checkAllCamps()
+  .controller('availabilityController', function (avService, $http) {
+    // let $ctrl = this;
+    // $ctrl.test = 'testing 123'
+    
+    this.camps = avService.checkAllCamps();
+    console.log(this.camps)
+    
   })
   .component(Component, { 
     template: template,
     controller: 'availabilityController'
-
   })
 
 exports[Component] = Component
+
