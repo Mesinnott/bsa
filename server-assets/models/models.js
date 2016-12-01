757b276d895f533256631b18b2bdd4bec106bcb6
Michael Sinnott <mesinnott@gmail.com>
2016-12-01 15:17:38 -0700

    distrcit fix

--- server-assets/models/models.js
+++ server-assets/models/models.js
@@ -537,8 +537,7 @@ let District = DS.defineResource({
 function districtCreate(district, cb) {
     let districtObj = {
         id: uuid.v4(),
-        name: district.name,
-        number: district.number
+        name: district.name
     }
 
     District.create(districtObj).then(cb).catch(cb)
