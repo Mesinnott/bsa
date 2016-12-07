ed0d9eb9b8b7af09674cf9d1d9764621676f6230
Michael Sinnott <mesinnott@gmail.com>
2016-12-07 15:20:17 -0700

    admin form

--- src/components/admin-component.js
+++ src/components/admin-component.js
@@ -14,7 +14,7 @@ angular.module(`app.components.${Component}`, [])
         ab.camps = [''];
         ab.scouts = [''];
         ab.reservation = [''];
-        ab.getCurrentYears = (cb)=>{
+        ab.getCurrentYears = (cb) => {
             let currentYear = new Date().getFullYear()
             currentYear = 2016
             let nextYear = currentYear++
@@ -99,6 +99,164 @@ angular.module(`app.components.${Component}`, [])
         let ad = this;
         ad.test = 'testing 123'
         ad.reservation = ['']
+        ad.prop=''
+        ad.resource='0'
+
+        ad.resources=[
+            {
+                name: "camps",
+                displayName: "Camps",
+                props: [
+                    {
+                        name: "campNum",
+                        displayName: "Camp Number"
+                    },
+                    {
+                        name: "date",
+                        displayName: "Date"
+                    },
+                    {
+                        name: "location",
+                        displayName: "Location"
+                    },
+                    {
+                        name: "maxScouts",
+                        displayName: "Max"
+                    },
+                    {
+                        name: "scoutLevels",
+                        displayName: "Scout Ages"
+                    },
+                ]
+            },
+            {
+                name: "packs",
+                displayName: "Packs",
+                props: [
+                    {
+                        name: "number",
+                        displayName: "Pack Number"
+                    },
+                    {
+                        name: "zone",
+                        displayName: "Pack Zone"
+                    },
+
+                ]
+            },
+            {
+                name:"scouts",
+                displayName: "Scouts",
+                props:[
+                    {
+                        name: "denNum",
+                        displayName: "Pack Number"
+                    },
+                    {
+                        name: "shirtSize",
+                        displayName: "Shirt Size"
+                    },
+                    {
+                        name: "reservationId",
+                        displayName: "Reservation Id"
+                    },
+                    {
+                        name: "name",
+                        displayName: "Name"
+                    }
+                ]
+            },
+            {
+                name:"leaders",
+                displayName: "Leaders",
+                props:[
+                    {
+                        name: "denNum",
+                        displayName: "Pack Number"
+                    },
+                    {
+                        name: "shirtSize",
+                        displayName: "Shirt Size"
+                    },
+                    {
+                        name: "reservationId",
+                        displayName: "Reservation Id"
+                    },
+                    {
+                        name: "name",
+                        displayName: "Name"
+                    }
+                ]
+
+            },
+            {
+                name: "directors",
+                displayName: "Camp Director",
+                props: [
+                    {
+                        name: "email",
+                        displayName: "Email"
+                    },
+                    {
+                        name: "name",
+                        displayName: "Name"
+                    }
+
+                ]
+            },
+            {
+                name: "reservations",
+                displayName: "Reservation",
+                props:[
+                    {
+                        name: "campNum",
+                        displayName: "Camp Number"
+                    },
+                    {
+                        name: "denNum",
+                        displayName: "Pack Number"
+                    },
+                    {
+                        name: "email",
+                        displayName: "Email Address"
+                    },
+                    {
+                        name: "id",
+                        displayName: "Reservation ID"
+                    }
+                ]
+            },
+            {
+                name: "districts",
+                displayName: "Districts",
+                props:[
+                    {
+                        name: "name",
+                        displayName: "Name"
+                    }
+                ]
+            },
+            {
+                name: "users",
+                displayName: "Users",
+                props: [
+                    {
+                        name: "displayName",
+                        displayName: "Name"
+                    },
+                    {
+                        name: "email",
+                        displayName: "Email"
+                    }
+
+                ]
+            }
+        ]
+
+
+
+
+
 
         this.getYears = function () {
             abService.getCurrentYears(
@@ -122,19 +280,27 @@ angular.module(`app.components.${Component}`, [])
         ad.reservations = function (value) {
             debugger
             console.log('its working...')
+            abService.getResByDen(ad.resource, ad.param, value, function (res) {
+                ad.reservation = res
+            })
 
-            if (value.length < 5) {
-                abService.getResByDen(ad.resource, ad.param, value, function(res){
-                    ad.reservation = res
-                })
-            }else{
             abService.reservationGetById(value, function (reserv) {
                 ad.resDetails = reserv;
             })
-            abService.scoutGetByAnyId(value, function (list) {
-                ad.reservation = list;
-            })
-        }
+
+
+            //     if (value.length < 5) {
+            //         abService.getResByDen(ad.resource, ad.param, value, function(res){
+            //             ad.reservation = res
+            //         })
+            //     }else{
+            //     abService.reservationGetById(value, function (reserv) {
+            //         ad.resDetails = reserv;
+            //     })
+            //     abService.scoutGetByAnyId(value, function (list) {
+            //         ad.reservation = list;
+            //     })
+            // }
 
         }
 
