a9988d6e1543283d94fcfe687c8f7995fb0fe438
Michael Sinnott <mesinnott@gmail.com>
2016-12-07 21:30:47 -0700

    group styles

--- src/components/viewCamp-component.js
+++ src/components/viewCamp-component.js
@@ -6,10 +6,32 @@ const Component = 'viewcamp';
 angular.module(`app.components.${Component}`, [])
 
     .service('viewcampService', function($http) {
+        var vs = this;
 
+        vs.getCamp = function (campId, cb) {
+            $http({
+                method: 'GET',
+                url: '/api/camps/' + campId
+            }).then(function (res) {
+                vs.camp = res.data;
+                return cb(vs.camp);
+            })
+        }
     })
 
     .controller('viewcampController', function(viewcampService, $http) {
+           let $ctrl = this;
+            var vc = this;
+            vc.camp=''
+
+           
+           this.getCamp = function(campId){
+            viewcampService.getCamp(campId, function(camp){
+                vc.camp = camp[0]
+                console.log(vc.camp)
+            })
+        }
+            this.getCamp("3554e413-d6b7-43eb-b12c-9264a20ee24f")
 
     })
 
